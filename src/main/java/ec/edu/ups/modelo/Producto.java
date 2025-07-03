f84787642a674554c59639bf8fcb70e69d81cf7e
Javier <javierbarrezueta1@gmail.com>
2025-07-03 09:40:19 -0500

    FEAT(UPS-0013 ): "Clase Poducto"

--- src/main/java/ec/edu/ups/modelo/Producto.java
+++ src/main/java/ec/edu/ups/modelo/Producto.java
@@ -5,24 +5,9 @@ public class Producto {
     private String nombre;
     private double precio;
 
-    public Producto() {
-    }
-
     public Producto(int codigo, String nombre, double precio) {
         this.codigo = codigo;
         this.nombre = nombre;
-        this.precio = precio;
-    }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setPrecio(double precio) {
         this.precio = precio;
     }
 
@@ -30,17 +15,32 @@ public class Producto {
         return codigo;
     }
 
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public double getPrecio() {
         return precio;
     }
 
-    @Override
-    public String toString() {
-        return nombre + " - $" + precio;
+    public void setPrecio(double precio) {
+        this.precio = precio;
     }
 
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
 }
