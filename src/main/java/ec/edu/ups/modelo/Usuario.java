af2ec58e6bf233d608bceb058a45d3c2dc7a2517
Javier <javierbarrezueta1@gmail.com>
2025-07-03 09:30:06 -0500

    FEAT(UPS-007): "Clase Usuario"

---
+++ src/main/java/ec/edu/ups/modelo/Usuario.java
@@ -0,0 +1,81 @@
+package ec.edu.ups.modelo;
+
+import java.util.GregorianCalendar;
+
+public class Usuario {
+    private String username;
+    private String contrasenia;
+    private Rol rol;
+    private String nombre;
+    private String celular;
+    private GregorianCalendar fecha;
+    private String email;
+
+    public Usuario(String nombreDeUsuario, String contrasenia, Rol rol, String nombre,
+                   String celular, GregorianCalendar fecha, String email) {
+        this.username = nombreDeUsuario;
+        this.contrasenia = contrasenia;
+        this.rol = rol;
+        this.nombre = nombre;
+        this.celular = celular;
+        this.fecha = fecha;
+        this.email = email;
+    }
+    public Usuario() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public GregorianCalendar getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(GregorianCalendar fecha) {
+        this.fecha = fecha;
+    }
+}
