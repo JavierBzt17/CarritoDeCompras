bbe4e441ded677f77fa5b36e2a12ca76a26f6971
Javier <javierbarrezueta1@gmail.com>
2025-07-03 09:33:43 -0500

    FEAT(UPS-0010): "Clase Item Carrito"

---
+++ src/main/java/ec/edu/ups/modelo/ItemCarrito.java
@@ -0,0 +1,35 @@
+package ec.edu.ups.modelo;
+
+public class ItemCarrito {
+    private Producto producto;
+    private int cantidad;
+
+    public ItemCarrito(Producto producto, int cantidad) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemCarrito{" +
+                "producto=" + producto +
+                ", cantidad=" + cantidad +
+                '}';
+    }
+}
