f28e53468af99fc93e02bba12177a14b560b2508
Javier <javierbarrezueta1@gmail.com>
2025-07-06 14:41:38 -0500

    FEAT(UPS-028):"Actualizacion"

--- src/main/java/ec/edu/ups/modelo/Pregunta.java
+++ src/main/java/ec/edu/ups/modelo/Pregunta.java
@@ -3,19 +3,17 @@ import ec.edu.ups.util.MensajeInternacionalizacionHandler;
 
 public enum Pregunta {
     COLOR_FAVORITO("pregunta.color_favorito"),
-    PRIMERA_MASCOTA("pregunta.primera_mascota"),
+    NOMBRE_MASCOTA("pregunta.nombre_mascota"),
     COMIDA_FAVORITA("pregunta.comida_favorita"),
     CIUDAD_NACIMIENTO("pregunta.ciudad_nacimiento"),
-    PROFESOR_FAVORITO("pregunta.profesor_favorito"),
-    CANCION_FAVORITA("pregunta.cancion_favorita"),
+    CANTANTE_FAVORITO("pregunta.cantante_favorito"),
     NOMBRE_PRIMER_AMIGO("pregunta.nombre_primer_amigo"),
     PELICULA_FAVORITA("pregunta.pelicula_favorita"),
     NOMBRE_MADRE("pregunta.nombre_madre"),
     NOMBRE_PADRE("pregunta.nombre_padre"),
     NOMBRE_CARINO("pregunta.nombre_carino"),
-    OBJETO_PERSONAL("pregunta.objeto_personal"),
     CANTIDAD_HERMANOS("pregunta.cantidad_hermanos"),
-    NOMBRE_PRIMERA_ESCUELA("pregunta.nombre_primera_escuela");
+    NOMBRE_COLEGIO("pregunta.nombre_colegio");
 
     private String enunciado;
     private MensajeInternacionalizacionHandler mi;
